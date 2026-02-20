7b978d264574fe8ab20ecf962961626f57d389f1
Sufal Thakre <sufalthakre1@gmail.com>
2026-02-20 10:03:15 +0530

    setup with database mongodb and mongoose and check health of api

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import dbConnect from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+
+dbConnect();
+
+app.use(cors());
+app.use(express.json());
+
+
+// Health check
+app.get("/", (req, res) => {
+  res.json({ message: "Invoice API is running 🚀"});
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
